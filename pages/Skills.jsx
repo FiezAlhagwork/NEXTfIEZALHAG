f3c2af8d34c9b5a9716422f22a438f94426ed917
FiezAlhagwork <148772477+FiezAlhagwork@users.noreply.github.com>
2024-04-02 18:37:58 +0300

    Update Skills.jsx

--- pages/Skills.jsx
+++ pages/Skills.jsx
@@ -5,7 +5,7 @@ import Skil from "./Skil";
 const Skills = () => {
   const skillsArray = [
     { id: 1, name: "Html", class: styles.html },
-    { id: 2, name: "REACT JS", class: styles.react },
+    { id: 2, name: "React", class: styles.react },
     { id: 3, name: "javascript", class: styles.javascript },
     { id: 4, name: "python", class: styles.python },
     { id: 5, name: "CSS", class: styles.css },
