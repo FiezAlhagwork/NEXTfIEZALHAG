04c8d809124458fb7e18f24c9d89013303e9a962
FiezAlhagwork <148772477+FiezAlhagwork@users.noreply.github.com>
2024-04-02 18:29:11 +0300

    init

---
+++ pages/Skills.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "@/styles/Home.module.css";
+
+const Skills = () => {
+  const skillsArray = [
+    { id: 1, name: "Html", class: styles.html },
+    { id: 2, name: "REACT JS", class: styles.react },
+    { id: 3, name: "javascript", class: styles.javascript },
+    { id: 4, name: "python", class: styles.python },
+    { id: 5, name: "CSS", class: styles.css },
+  ];
+
+  return (
+    <div className={styles.skills__bar}>
+      {skillsArray.map((items) => {
+        return (
+          <div className={styles.bar}>
+            <div className={styles.info}>
+              <span>{items.name}</span>
+            </div>
+            <div className={`${styles.progress__line} ${items.class}`}>
+              <span></span>
+            </div>
+          </div>
+        );
+      })}
+      </div>
+
+
+   
+  );
+};
+
+export default Skills;
